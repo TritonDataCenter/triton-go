0c4cdddbac2cb11a312707e85fb587cb75506d63
James Nugent <james@jen20.com>
2017-01-18 14:43:34 +0000

    networks: Add GetNetwork operation

--- networks.go
+++ networks.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"fmt"
 	"github.com/hashicorp/errwrap"
 )
 
@@ -47,6 +48,29 @@ func (client *NetworksClient) ListNetworks(*ListNetworksInput) ([]*Network, erro
 	decoder := json.NewDecoder(respReader)
 	if err = decoder.Decode(&result); err != nil {
 		return nil, errwrap.Wrapf("Error decoding ListNetworks response: {{err}}", err)
+	}
+
+	return result, nil
+}
+
+type GetNetworkInput struct {
+	ID string
+}
+
+func (client *NetworksClient) GetNetwork(input *GetNetworkInput) (*Network, error) {
+	path := fmt.Sprintf("/%s/networks/%s", client.accountName, input.ID)
+	respReader, err := client.executeRequest(http.MethodGet, path, nil)
+	if respReader != nil {
+		defer respReader.Close()
+	}
+	if err != nil {
+		return nil, errwrap.Wrapf("Error executing GetNetwork request: {{err}}", err)
+	}
+
+	var result *Network
+	decoder := json.NewDecoder(respReader)
+	if err = decoder.Decode(&result); err != nil {
+		return nil, errwrap.Wrapf("Error decoding GetNetwork response: {{err}}", err)
 	}
 
 	return result, nil
