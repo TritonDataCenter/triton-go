dc122907a75dc27058a30c6b7161274ee56c85f0
Justin Reagor <justin.reagor@joyent.com>
2017-06-14 17:33:51 -0400

    Bring back functionality of the current root client module

--- triton.go
+++ triton.go
@@ -1,6 +1,7 @@
 package triton
 
 import (
+	"github.com/joyent/triton-go/authentication"
 	"github.com/joyent/triton-go/client"
 	"github.com/joyent/triton-go/compute"
 	"github.com/joyent/triton-go/storage"
@@ -8,27 +9,26 @@ import (
 
 type TritonClient struct {
 	*client.Client
-	Compute compute.Compute
-	Storage storage.Storage
+	Compute *compute.Compute
+	Storage *storage.Storage
 }
 
 type ClientConfig struct {
 	*client.Config
+	Endpoint    string
+	AccountName string
+	Signers     []authentication.Signer
 }
 
 // TODO: Work configuration providers into the mix for pulling variables out of
 // the env or `node-triton` profile.
 func NewClient(config *ClientConfig) (*TritonClient, error) {
 	// TODO: Utilize config interface within the function itself
-	client, error := client.New(config.endpoint, config.accountName, config.signers)
+	client, error := client.New(config.Endpoint, config.AccountName, config.Signers...)
 	if error != nil {
 		return nil, error
 	}
-	compute := &Compute{
-		client: client,
-	}
-	storage := &Storage{
-		client: client,
-	}
-	return &TritonClient{client, compute, storage}
+	compute := &compute.Compute{client}
+	storage := &storage.Storage{client}
+	return &TritonClient{client, compute, storage}, nil
 }
