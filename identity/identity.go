6cec226b1ca3ac21c55f17df9877e21b831055b0
Justin Reagor <justin.reagor@joyent.com>
2017-06-14 17:31:42 -0400

    Refactor in identity package

--- identity/identity.go
+++ identity/identity.go
@@ -5,3 +5,9 @@ import "github.com/joyent/triton-go/client"
 type Identity struct {
 	client *client.Client
 }
+
+// Roles returns a Compute client used for accessing functions pertaining to
+// Role functionality in the Triton API.
+func (c *Identity) Roles() *RolesClient {
+	return &RolesClient{c}
+}
--- identity/roles.go
+++ identity/roles.go
@@ -1,4 +1,4 @@
-package compute
+package identity
 
 import (
 	"context"
@@ -10,7 +10,7 @@ import (
 )
 
 type RolesClient struct {
-	*Compute
+	*Identity
 }
 
 type Role struct {
