7af43fa5ac68aa4161826fdecf7fccadf8af8149
Sam Gwydir <sam@samgwydir.com>
2017-07-24 16:23:37 -0700

    Update examples for new api

--- examples/storage/mls.go
+++ examples/storage/mls.go
@@ -2,47 +2,41 @@ package main
 
 import (
 	"context"
-	"io/ioutil"
+	"fmt"
 	"log"
 	"os"
 
-	"github.com/davecgh/go-spew/spew"
 	triton "github.com/joyent/triton-go"
 	"github.com/joyent/triton-go/authentication"
 	"github.com/joyent/triton-go/storage"
 )
 
 func main() {
-	keyID := os.Getenv("SDC_KEY_ID")
-	accountName := os.Getenv("SDC_ACCOUNT")
-	keyPath := os.Getenv("SDC_KEY_FILE")
+	keyID := os.Getenv("MANTA_KEY_ID")
+	accountName := os.Getenv("MANTA_USER")
+	mantaURL := os.Getenv("MANTA_URL")
 
-	privateKey, err := ioutil.ReadFile(keyPath)
+	sshKeySigner, err := authentication.NewSSHAgentSigner(keyID, accountName)
 	if err != nil {
-		log.Fatalf("Couldn't find key file matching %s\n%s", keyID, err)
+		log.Fatalf("NewSSHAgentSigner: %s", err)
 	}
 
-	sshKeySigner, err := authentication.NewPrivateKeySigner(keyID, privateKey, accountName)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	config := &triton.ClientConfig{
-		MantaURL:    os.Getenv("MANTA_URL"),
+	client, err := storage.NewClient(&triton.ClientConfig{
+		MantaURL:    mantaURL,
 		AccountName: accountName,
 		Signers:     []authentication.Signer{sshKeySigner},
-	}
-	client, err := storage.NewClient(config)
+	})
 	if err != nil {
 		log.Fatalf("NewClient: %s", err)
 	}
 
-	ctx := context.Background()
-	input := &storage.ListDirectoryInput{}
-	output, err := client.Dir().List(ctx, input)
+	output, err := client.Dir().List(context.Background(), &storage.ListDirectoryInput{
+		DirectoryName: "/stor",
+	})
 	if err != nil {
-		log.Fatalf("storage.Dir.List: %s", err)
+		log.Fatalf("ListDirectory(): %s", err)
 	}
-
-	spew.Dump(output)
+	for _, entry := range output.Entries {
+		fmt.Println(entry.Name)
+	}
 }
