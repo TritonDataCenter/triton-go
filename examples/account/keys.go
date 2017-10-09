f4aa7439ff2ac54baa8a0596d20256c2e9395772
stack72 <public@paulstack.co.uk>
2017-10-09 15:06:32 +0300

    Unify the way we tell people how to initialize client configuration
    This supports the 3 keys ways:
    
    * SSH Agent signer
    * Passing a path to an SSH Key
    * Passing the contents of an SSH KEy

--- examples/account/keys.go
+++ examples/account/keys.go
@@ -6,34 +6,59 @@ import (
 	"log"
 	"os"
 
+	"encoding/pem"
+	"io/ioutil"
+
 	triton "github.com/joyent/triton-go"
 	"github.com/joyent/triton-go/account"
 	"github.com/joyent/triton-go/authentication"
 )
 
 func main() {
-	keyID, foundKey := os.LookupEnv("SDC_KEY_ID")
-	if !foundKey {
-		log.Fatal("Couldn't find \"SDC_KEY_ID\" in your environment")
-	}
+	keyID := os.Getenv("SDC_KEY_ID")
+	accountName := os.Getenv("SDC_ACCOUNT")
+	keyMaterial := os.Getenv("SDC_KEY_MATERIAL")
 
-	accountName, foundAccount := os.LookupEnv("SDC_ACCOUNT")
-	if !foundAccount {
-		log.Fatal("Couldn't find \"SDC_ACCOUNT\" in your environment")
-	}
+	var signer authentication.Signer
+	var err error
 
-	tritonURL, foundURL := os.LookupEnv("SDC_URL")
-	if !foundURL {
-		log.Fatal("Couldn't find \"SDC_URL\" in your environment")
-	}
+	if keyMaterial == "" {
+		signer, err = authentication.NewSSHAgentSigner(keyID, accountName)
+		if err != nil {
+			log.Fatalf("Error Creating SSH Agent Signer: {{err}}", err)
+		}
+	} else {
+		var keyBytes []byte
+		if _, err = os.Stat(keyMaterial); err == nil {
+			keyBytes, err = ioutil.ReadFile(keyMaterial)
+			if err != nil {
+				log.Fatalf("Error reading key material from %s: %s",
+					keyMaterial, err)
+			}
+			block, _ := pem.Decode(keyBytes)
+			if block == nil {
+				log.Fatalf(
+					"Failed to read key material '%s': no key found", keyMaterial)
+			}
 
-	signer, err := authentication.NewSSHAgentSigner(keyID, accountName)
-	if err != nil {
-		log.Fatal(err)
+			if block.Headers["Proc-Type"] == "4,ENCRYPTED" {
+				log.Fatalf(
+					"Failed to read key '%s': password protected keys are\n"+
+						"not currently supported. Please decrypt the key prior to use.", keyMaterial)
+			}
+
+		} else {
+			keyBytes = []byte(keyMaterial)
+		}
+
+		signer, err = authentication.NewPrivateKeySigner(keyID, []byte(keyMaterial), accountName)
+		if err != nil {
+			log.Fatalf("Error Creating SSH Private Key Signer: {{err}}", err)
+		}
 	}
 
 	config := &triton.ClientConfig{
-		TritonURL:   tritonURL,
+		TritonURL:   os.Getenv("SDC_URL"),
 		AccountName: accountName,
 		Signers:     []authentication.Signer{signer},
 	}
--- examples/compute/create_instance.go
+++ examples/compute/create_instance.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/pem"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
@@ -21,31 +23,54 @@ const (
 )
 
 func main() {
-	keyID, foundKey := os.LookupEnv("SDC_KEY_ID")
-	if !foundKey {
-		log.Fatal("Couldn't find \"SDC_KEY_ID\" in your environment")
-	}
+	keyID := os.Getenv("SDC_KEY_ID")
+	accountName := os.Getenv("SDC_ACCOUNT")
+	keyMaterial := os.Getenv("SDC_KEY_MATERIAL")
 
-	accountName, foundAccount := os.LookupEnv("SDC_ACCOUNT")
-	if !foundAccount {
-		log.Fatal("Couldn't find \"SDC_ACCOUNT\" in your environment")
-	}
+	var signer authentication.Signer
+	var err error
 
-	tritonURL, foundURL := os.LookupEnv("SDC_URL")
-	if !foundURL {
-		log.Fatal("Couldn't find \"SDC_URL\" in your environment")
-	}
+	if keyMaterial == "" {
+		signer, err = authentication.NewSSHAgentSigner(keyID, accountName)
+		if err != nil {
+			log.Fatalf("Error Creating SSH Agent Signer: {{err}}", err)
+		}
+	} else {
+		var keyBytes []byte
+		if _, err = os.Stat(keyMaterial); err == nil {
+			keyBytes, err = ioutil.ReadFile(keyMaterial)
+			if err != nil {
+				log.Fatalf("Error reading key material from %s: %s",
+					keyMaterial, err)
+			}
+			block, _ := pem.Decode(keyBytes)
+			if block == nil {
+				log.Fatalf(
+					"Failed to read key material '%s': no key found", keyMaterial)
+			}
 
-	signer, err := authentication.NewSSHAgentSigner(keyID, accountName)
-	if err != nil {
-		log.Fatal(err)
+			if block.Headers["Proc-Type"] == "4,ENCRYPTED" {
+				log.Fatalf(
+					"Failed to read key '%s': password protected keys are\n"+
+						"not currently supported. Please decrypt the key prior to use.", keyMaterial)
+			}
+
+		} else {
+			keyBytes = []byte(keyMaterial)
+		}
+
+		signer, err = authentication.NewPrivateKeySigner(keyID, []byte(keyMaterial), accountName)
+		if err != nil {
+			log.Fatalf("Error Creating SSH Private Key Signer: {{err}}", err)
+		}
 	}
 
 	config := &triton.ClientConfig{
-		TritonURL:   tritonURL,
+		TritonURL:   os.Getenv("SDC_URL"),
 		AccountName: accountName,
 		Signers:     []authentication.Signer{signer},
 	}
+
 	c, err := compute.NewClient(config)
 	if err != nil {
 		log.Fatalf("Compute NewClient(): %s", err)
