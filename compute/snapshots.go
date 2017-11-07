3175dfdd4e069da5947b4dd1cefd0a41e5fff812
stack72 <public@paulstack.co.uk>
2017-11-07 15:16:12 +0200

    Add support for CreateSnapshot and StartMachineFromSnapshot endpoints
    Add the missing endpoints for snapshot management

--- compute/snapshots.go
+++ compute/snapshots.go
@@ -99,3 +99,59 @@ func (c *SnapshotsClient) Delete(ctx context.Context, input *DeleteSnapshotInput
 
 	return nil
 }
+
+type StartMachineFromSnapshotInput struct {
+	MachineID string
+	Name      string
+}
+
+func (c *SnapshotsClient) StartMachine(ctx context.Context, input *StartMachineFromSnapshotInput) error {
+	path := fmt.Sprintf("/%s/machines/%s/snapshots/%s", c.client.AccountName, input.MachineID, input.Name)
+	reqInputs := client.RequestInput{
+		Method: http.MethodPost,
+		Path:   path,
+	}
+	respReader, err := c.client.ExecuteRequest(ctx, reqInputs)
+	if respReader != nil {
+		defer respReader.Close()
+	}
+	if err != nil {
+		return errwrap.Wrapf("Error executing StartMachine request: {{err}}", err)
+	}
+
+	return nil
+}
+
+type CreateSnapshotInput struct {
+	MachineID string
+	Name      string
+}
+
+func (c *SnapshotsClient) Create(ctx context.Context, input *CreateSnapshotInput) (*Snapshot, error) {
+	path := fmt.Sprintf("/%s/machines/%s/snapshots", c.client.AccountName, input.MachineID)
+
+	data := make(map[string]interface{})
+	data["name"] = input.Name
+
+	reqInputs := client.RequestInput{
+		Method: http.MethodPost,
+		Path:   path,
+		Body:   data,
+	}
+
+	respReader, err := c.client.ExecuteRequest(ctx, reqInputs)
+	if respReader != nil {
+		defer respReader.Close()
+	}
+	if err != nil {
+		return nil, errwrap.Wrapf("Error executing Create request: {{err}}", err)
+	}
+
+	var result *Snapshot
+	decoder := json.NewDecoder(respReader)
+	if err = decoder.Decode(&result); err != nil {
+		return nil, errwrap.Wrapf("Error decoding Create response: {{err}}", err)
+	}
+
+	return result, nil
+}
--- compute/snapshots_test.go
+++ compute/snapshots_test.go
@@ -17,9 +17,11 @@ import (
 const accountUrl = "testing"
 
 var (
-	listSnapshotErrorType   = errors.New("Error executing List request:")
-	getSnapshotErrorType    = errors.New("Error executing Get request:")
-	deleteSnapshotErrorType = errors.New("Error executing Delete request:")
+	listSnapshotErrorType             = errors.New("Error executing List request:")
+	getSnapshotErrorType              = errors.New("Error executing Get request:")
+	deleteSnapshotErrorType           = errors.New("Error executing Delete request:")
+	createSnapshotErrorType           = errors.New("Error executing Create request:")
+	startMachineFromSnapshotErrorType = errors.New("Error executing StartMachine request:")
 )
 
 func MockIdentityClient() *compute.ComputeClient {
@@ -208,6 +210,80 @@ func TestDeleteSnapshot(t *testing.T) {
 	})
 }
 
+func TestStartMachineFromSnapshot(t *testing.T) {
+	computeClient := MockIdentityClient()
+
+	do := func(ctx context.Context, cc *compute.ComputeClient) error {
+		defer testutils.DeactivateClient()
+
+		return cc.Snapshots().StartMachine(ctx, &compute.StartMachineFromSnapshotInput{
+			MachineID: "123-3456-2335",
+			Name:      "sample-snapshot",
+		})
+	}
+
+	t.Run("successful", func(t *testing.T) {
+		testutils.RegisterResponder("POST", fmt.Sprintf("/%s/machines/%s/snapshots/%s", accountUrl, "123-3456-2335", "sample-snapshot"), startMachineFromSnapshotSuccess)
+
+		err := do(context.Background(), computeClient)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("error", func(t *testing.T) {
+		testutils.RegisterResponder("POST", fmt.Sprintf("/%s/machines/%s/snapshots/%s", accountUrl, "123-3456-2335", "sample-snapshot"), startMachineFromSnapshotError)
+
+		err := do(context.Background(), computeClient)
+		if err == nil {
+			t.Fatal(err)
+		}
+
+		if !strings.Contains(err.Error(), "Error executing StartMachine request:") {
+			t.Errorf("expected error to equal testError: found %s", err)
+		}
+	})
+}
+
+func TestCreateSnapshot(t *testing.T) {
+	computeClient := MockIdentityClient()
+
+	do := func(ctx context.Context, cc *compute.ComputeClient) (*compute.Snapshot, error) {
+		defer testutils.DeactivateClient()
+
+		snapshot, err := cc.Snapshots().Create(ctx, &compute.CreateSnapshotInput{
+			MachineID: "123-3456-2335",
+			Name:      "sample-snapshot",
+		})
+		if err != nil {
+			return nil, err
+		}
+		return snapshot, nil
+	}
+
+	t.Run("successful", func(t *testing.T) {
+		testutils.RegisterResponder("POST", fmt.Sprintf("/%s/machines/%s/snapshots", accountUrl, "123-3456-2335"), createSnapshotSuccess)
+
+		_, err := do(context.Background(), computeClient)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("error", func(t *testing.T) {
+		testutils.RegisterResponder("POST", fmt.Sprintf("/%s/machines/%s/snapshots", accountUrl, "123-3456-2335"), createSnapshotError)
+
+		_, err := do(context.Background(), computeClient)
+		if err == nil {
+			t.Fatal(err)
+		}
+
+		if !strings.Contains(err.Error(), "Error executing Create request:") {
+			t.Errorf("expected error to equal testError: found %s", err)
+		}
+	})
+}
+
 func listSnapshotsSuccess(req *http.Request) (*http.Response, error) {
 	header := http.Header{}
 	header.Add("Content-Type", "application/json")
@@ -323,4 +399,41 @@ func deleteSnapshotSuccess(req *http.Request) (*http.Response, error) {
 
 func deleteSnapshotError(req *http.Request) (*http.Response, error) {
 	return nil, deleteSnapshotErrorType
+}
+
+func startMachineFromSnapshotSuccess(req *http.Request) (*http.Response, error) {
+	header := http.Header{}
+	header.Add("Content-Type", "application/json")
+
+	return &http.Response{
+		StatusCode: 202,
+		Header:     header,
+	}, nil
+}
+
+func startMachineFromSnapshotError(req *http.Request) (*http.Response, error) {
+	return nil, startMachineFromSnapshotErrorType
+}
+
+func createSnapshotSuccess(req *http.Request) (*http.Response, error) {
+	header := http.Header{}
+	header.Add("Content-Type", "application/json")
+
+	body := strings.NewReader(`{
+    "name": "sample-snapshot",
+	"state": "queued",
+    "updated": "2015-12-23T06:41:11.032Z",
+    "created": "2015-12-23T06:41:11.032Z"
+  }
+`)
+
+	return &http.Response{
+		StatusCode: 201,
+		Header:     header,
+		Body:       ioutil.NopCloser(body),
+	}, nil
+}
+
+func createSnapshotError(req *http.Request) (*http.Response, error) {
+	return nil, createSnapshotErrorType
 }
