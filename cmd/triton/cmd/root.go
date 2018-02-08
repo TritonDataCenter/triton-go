e50afbb9fb94561bf0af5be7f2648b97a1ff223f
stack72 <public@paulstack.co.uk>
2018-02-08 14:48:44 +0200

    WIP to finish List instances

--- cmd/triton/cmd/root.go
+++ cmd/triton/cmd/root.go
@@ -3,27 +3,52 @@ package cmd
 import (
 	"os"
 
+	"github.com/joyent/triton-go/cmd/internal/command"
 	"github.com/joyent/triton-go/cmd/internal/config"
 	"github.com/joyent/triton-go/cmd/internal/console_writer"
 	"github.com/joyent/triton-go/cmd/internal/logger"
-	"github.com/joyent/triton-go/cmd/triton/cmd/account"
 	"github.com/joyent/triton-go/cmd/triton/cmd/compute"
-	"github.com/joyent/triton-go/cmd/triton/cmd/identity"
-	"github.com/joyent/triton-go/cmd/triton/cmd/network"
-	"github.com/joyent/triton-go/cmd/triton/cmd/version"
 	isatty "github.com/mattn/go-isatty"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "triton",
-	Short: "cli for interacting with triton",
+var subCommands = []*command.Command{
+	compute.InstancesCommand,
+}
+
+var rootCmd = &command.Command{
+	Cobra: &cobra.Command{
+		Use:   "triton",
+		Short: "cli for interacting with triton",
+	},
+	Setup: func(parent *command.Command) error {
+		{
+			const (
+				key         = config.KeyUsePager
+				longName    = "use-pager"
+				shortName   = "P"
+				description = "Use a pager to read the output (defaults to $PAGER, less(1), or more(1))"
+			)
+			var defaultValue bool
+			if isatty.IsTerminal(os.Stdout.Fd()) || isatty.IsCygwinTerminal(os.Stdout.Fd()) {
+				defaultValue = true
+			}
+
+			flags := parent.Cobra.PersistentFlags()
+			flags.BoolP(longName, shortName, defaultValue, description)
+			viper.BindPFlag(key, flags.Lookup(longName))
+			viper.SetDefault(key, defaultValue)
+		}
+
+		return nil
+	},
 }
 
 func Execute() error {
-	initRootFlags()
+
+	rootCmd.Setup(rootCmd)
 
 	console_writer.UsePager(viper.GetBool(config.KeyUsePager))
 
@@ -31,41 +56,15 @@ func Execute() error {
 		return err
 	}
 
-	rootCmd.AddCommand(account.AccountCommand)
-	rootCmd.AddCommand(identity.IdentityCommand)
-	rootCmd.AddCommand(network.NetworkCommand)
-	rootCmd.AddCommand(version.Cmd)
+	for _, cmd := range subCommands {
+		rootCmd.Cobra.AddCommand(cmd.Cobra)
+		cmd.Setup(cmd)
+	}
 
-	account.SetUpCommands()
-	compute.SetUpCommands(rootCmd)
-	identity.SetUpCommands()
-	network.SetUpCommands()
-
-	if err := rootCmd.Execute(); err != nil {
+	if err := rootCmd.Cobra.Execute(); err != nil {
 		log.Error().Err(err).Msg("unable to run")
 		return err
 	}
 
 	return nil
-}
-
-func initRootFlags() {
-	{
-		const (
-			key         = config.KeyUsePager
-			longName    = "use-pager"
-			shortName   = "P"
-			description = "Use a pager to read the output (defaults to $PAGER, less(1), or more(1))"
-		)
-		var defaultValue bool
-		if isatty.IsTerminal(os.Stderr.Fd()) || isatty.IsCygwinTerminal(os.Stderr.Fd()) {
-			defaultValue = true
-		}
-
-		f := rootCmd.PersistentFlags()
-		f.BoolP(longName, shortName, defaultValue, description)
-		viper.BindPFlag(key, f.Lookup(longName))
-		viper.BindEnv(key, "TRITON_USE_PAGER")
-		viper.SetDefault(key, defaultValue)
-	}
 }
