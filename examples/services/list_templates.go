84ad891a590ef2a4506101a2437c24fdaf22a502
Justin Reagor <justin.reagor@joyent.com>
2018-02-28 20:46:04 -0500

    services: Update TSG API example file

--- examples/services/list_templates.go
+++ examples/services/list_templates.go
@@ -77,15 +77,75 @@ func main() {
 
 	s, err := services.NewClient(config)
 	if err != nil {
-		log.Fatalf("failed to create new service groups client: %v", err)
+		log.Fatalf("failed to create new services client: %v", err)
 	}
 
-	input := &services.ListTemplatesInput{}
-	templates, err := s.Templates().List(context.Background(), input)
+	listInput := &services.ListTemplatesInput{}
+	templates, err := s.Templates().List(context.Background(), listInput)
 	if err != nil {
-		log.Fatalf("failed to list machine templates: %v", err)
+		log.Fatalf("failed to list instance templates: %v", err)
 	}
 	for _, template := range templates {
-		fmt.Println("Template Name", template.Name)
+		fmt.Printf("Template Name: %s\n", template.TemplateName)
+		fmt.Printf("ID: %v\n", template.ID)
+		fmt.Printf("AccountId: %v\n", template.AccountId)
+		fmt.Printf("Package: %v\n", template.Package)
+		fmt.Printf("ImageId: %v\n", template.ImageId)
+		fmt.Printf("InstanceNamePrefix: %v\n", template.InstanceNamePrefix)
+		fmt.Printf("FirewallEnabled: %v\n", template.FirewallEnabled)
+		fmt.Printf("Networks: %v\n", template.Networks)
+		fmt.Printf("UserData: %v\n", template.UserData)
+		fmt.Printf("MetaData: %v\n", template.MetaData)
+		fmt.Printf("Tags: %v\n", template.Tags)
+		fmt.Println("")
 	}
+
+	fmt.Println("---")
+
+	if tmpl := templates[0]; tmpl != nil {
+		getInput := &services.GetTemplateInput{
+			Name: tmpl.TemplateName,
+		}
+		template, err := s.Templates().Get(context.Background(), getInput)
+		if err != nil {
+			log.Fatalf("failed to get instance template: %v", err)
+		}
+
+		fmt.Printf("Got Template: %s\n", template.TemplateName)
+	}
+
+	fmt.Println("---")
+
+	customTemplateName := "custom-template-1"
+
+	createInput := &services.CreateTemplateInput{
+		TemplateName:       customTemplateName,
+		AccountId:          "joyent",
+		Package:            "test-package",
+		ImageId:            "49b22aec-0c8a-11e6-8807-a3eb4db576ba",
+		InstanceNamePrefix: "sample-",
+		FirewallEnabled:    false,
+		Networks:           []string{"f7ed95d3-faaf-43ef-9346-15644403b963"},
+		UserData:           "bash script here",
+		MetaData:           map[string]string{"metadata": "test"},
+		Tags:               map[string]string{"tag": "test"},
+	}
+	err = s.Templates().Create(context.Background(), createInput)
+	if err != nil {
+		log.Fatalf("failed to create instance template: %v", err)
+	}
+
+	fmt.Printf("Created Template: %s\n", customTemplateName)
+	fmt.Println("---")
+
+	deleteInput := &services.DeleteTemplateInput{
+		Name: customTemplateName,
+	}
+	err = s.Templates().Delete(context.Background(), deleteInput)
+	if err != nil {
+		log.Fatalf("failed to delete instance template: %v", err)
+	}
+
+	fmt.Printf("Delete Template: %s\n", customTemplateName)
+
 }
