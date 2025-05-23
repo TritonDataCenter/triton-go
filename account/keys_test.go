7edea8b3583fdca21fbe236f2f54416d5b4cbbae
Jhonas Wernery <jhonas.wernery@mnxsolutions.com>
2025-05-23 16:53:36 +0200

    add copyright line

--- account/keys_test.go
+++ account/keys_test.go
@@ -1,5 +1,6 @@
 //
 // Copyright 2020 Joyent, Inc. All rights reserved.
+// Copyright 2025 MNX Cloud, Inc.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
